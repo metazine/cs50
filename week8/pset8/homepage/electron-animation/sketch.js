e02d4687f53f7bcdb4b7e0f2a37fcaa09f2c5e14
Luke <lukequattrope@protonmail.com>
2022-05-28 21:23:35 +0100

    renamed variables sketch.js

--- week8/pset8/homepage/electron-animation/sketch.js
+++ week8/pset8/homepage/electron-animation/sketch.js
@@ -1,5 +1,5 @@
 const size = 50
-const speed = 2
+const speed = 100
 
 
 function setup() {
@@ -7,7 +7,7 @@ function setup() {
     frameRate(60)
 }
 
-function electron(radius, electron_count) {  
+function electron_ring(radius, electron_count) {  
     this.radius = radius
     this.diameter = radius * 2
     this.electron_count = electron_count;
@@ -15,19 +15,20 @@ function electron(radius, electron_count) {
     this.width = 20
 
     this.draw = function() {
-        const centre_x = width/2
-        const centre_y = height/2
+        const cx = width/2
+        const cy = height/2
         const spacing = 360 / this.electron_count
         stroke(0)
         noFill()
-        circle(centre_x, centre_y, this.diameter)
+        circle(cx, cy, this.diameter)
 
         for (let i = 0; i < this.electron_count; i ++) {  
             this.calculate_position((i * spacing) + this.angle)
             noStroke()
             fill(255, 255, 0)
-            ellipse(centre_x + this.x, centre_y + this.y, this.width, this.width)
+            ellipse(cx + this.x, cy + this.y, this.width, this.width)
         }
+        this.angle += speed/radius
     }
 
     this.calculate_position = function(degree_offset) {
@@ -42,10 +43,11 @@ function to_radian(angle) {
     return angle * (Math.PI / 180)
 }
 
-const my_electron = new electron(180, 4)
+const electron_outer_ring = new electron_ring(180, 8)
+const electron_inner_ring = new electron_ring(50, 2)
 
 function draw() {
     background(220);
-    my_electron.draw()
-    my_electron.angle += speed  
+    electron_outer_ring.draw()
+    electron_inner_ring.draw()
 }
