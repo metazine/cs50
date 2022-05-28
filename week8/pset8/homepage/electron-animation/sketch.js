78151f53202641f29eb47a460726d0aafda0438d
Luke <lukequattrope@protonmail.com>
2022-05-28 20:06:36 +0100

    sketch.js degree/radian fix

--- week8/pset8/homepage/electron-animation/sketch.js
+++ week8/pset8/homepage/electron-animation/sketch.js
@@ -4,43 +4,43 @@ const size = 50
 
 function setup() {
     createCanvas(400, 400);
-    frameRate(120)
+    frameRate(60)
 }
 
-function electron(radius) {  
+function electron(radius, electron_count) {  
     this.radius = radius
+    this.electron_count = electron_count;
     this.angle = 0
-
     this.width = 20
 
-    
-
     this.draw = function() {
+        const centre_x = width/2
+        const centre_y = height/2
+        console.log("frame_start")
+        for (let i = 0; i < this.electron_count; i ++) {  
+            this.calculate_position((i * 90) + this.angle)
+            noStroke()
+            fill(255, 255, 0)
+            ellipse(centre_x + this.x, centre_y + this.y, this.width, this.width)
+        }
+    }
+
+    this.calculate_position = function(degree_offset) {
+        const radian_offset = to_radian(degree_offset)
         
-        this.calculate_position()
-        ellipse(width/2 + this.x, height/2 + this.y, this.width, this.width)
-    }
-
-    this.calculate_position = function() {
-        this.x = sin(this.angle) * this.radius
-        this.y = -cos(this.angle) * this.radius
-    }
-
-    this.orbit_cx = function (position, size) {
-        return(sin(position) * size)
-    }
-
-    this.orbit_cy = function (position, size) {
-        return(cos(position) * size)
+        this.x = Math.sin(radian_offset) * this.radius
+        this.y = -Math.cos(radian_offset) * this.radius
     }
 }
 
-const my_electron = new electron(200)
+function to_radian(angle) {
+    return angle * (Math.PI / 180)
+}
+
+const my_electron = new electron(200, 4)
 
 function draw() {
     background(220);
     my_electron.draw()
-    my_electron.angle+=0.05
-    count+=0.1
-  
+    my_electron.angle+=2  
 }
