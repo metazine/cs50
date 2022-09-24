ad0a7748d4cb4c1872040ad1cbb647937646bfaa
Luke <lukequattrope@protonmail.com>
2022-09-24 07:56:26 +0100

    refactored

--- week4/filter/redone_filter/filter.ts
+++ week4/filter/redone_filter/filter.ts
@@ -2,11 +2,20 @@ const fs = require('fs')
 const bmp = require('bmp-js')
 
 function main() {
+    // LOAD IN BMP
+    const inputBMPBuffer = fs.readFileSync(process.argv[3])
+    const inputBMPData = bmp.decode(inputBMPBuffer)
 
+    const rowsOfPixels = bmpDataToRowsOfPixels(inputBMPData)
+    const filteredRowsOfPixels = greyScale(rowsOfPixels)
+    const outputRGBdata = convertPixelArrayTo1DArray(filteredRowsOfPixels)
+    
+    const outputBMPData = inputBMPData
+    outputBMPData.data = outputRGBdata
+
+    const outputBMPBuffer = bmp.encode(outputBMPData)
+    fs.writeFileSync('output.bmp', outputBMPBuffer.data)
 }
-
-
-// PROCESSING INPUT
 
 interface Pixel {
     a: number,
@@ -32,31 +41,32 @@ interface DecodedBMP {
     importantColors: number,
     data: number[]
 }
-// LOAD IN BMP
-const inputBMPBuffer = fs.readFileSync(process.argv[3])
+
+interface FilterSpecs {
+    
+}
 
 // CONVERT BMP TO TWO DIMENSIONAL ARRAY
-function bmpBufferToRowsOfPixels() {
+function bmpDataToRowsOfPixels(bmpData): Pixel[][] {
     let rowsOfPixels: Pixel[][] = []
 
     const VALUES_PER_PIXEL = 4
-    for (let y = 0; y < inputBMPData.height; y++) {
+    for (let y = 0; y < bmpData.height; y++) {
         let row: Pixel[] = []
-        for (let x = 0; x < inputBMPData.width * VALUES_PER_PIXEL; x += VALUES_PER_PIXEL) {
-            const location = y * inputBMPData.width * VALUES_PER_PIXEL + x
+        for (let x = 0; x < bmpData.width * VALUES_PER_PIXEL; x += VALUES_PER_PIXEL) {
+            const location = y * bmpData.width * VALUES_PER_PIXEL + x
             let pixel: Pixel = {
-                a: inputBMPData.data[location],
-                r: inputBMPData.data[location + 3],
-                g: inputBMPData.data[location + 2],
-                b: inputBMPData.data[location + 1]
+                a: bmpData.data[location],
+                r: bmpData.data[location + 3],
+                g: bmpData.data[location + 2],
+                b: bmpData.data[location + 1]
             }
             row.push(pixel)
         }
         rowsOfPixels.push(row)
     }
+    return rowsOfPixels
 }
-const inputBMPData: DecodedBMP = bmp.decode(inputBMPBuffer)
-
 
 
 
@@ -65,7 +75,7 @@ function greyScale(rowsOfPixels: Pixel[][]) {
     for (let y = 0; y < rowsOfPixels.length; y++) {
         for (let x = 0; x < rowsOfPixels[y].length; x++) {
             const averageOfColours = (rowsOfPixels[y][x].r + rowsOfPixels[y][x].g + rowsOfPixels[y][x].b) / 3
-            rowsOfPixels[y][x].a = 1
+            rowsOfPixels[y][x].a = 0
             rowsOfPixels[y][x].r = averageOfColours
             rowsOfPixels[y][x].g = averageOfColours
             rowsOfPixels[y][x].b = averageOfColours
@@ -78,21 +88,25 @@ function greyScale(rowsOfPixels: Pixel[][]) {
 //rowsOfPixels = greyScale(rowsOfPixels)
 
 // CONVERT FILTERED 2D ARRAY TO A 1D ARRAY
-const outputBMPData: DecodedBMP = inputBMPData
-outputBMPData.data = []
+function convertPixelArrayTo1DArray(rowsOfPixels: Pixel[][]) {
+    let data: number[] = []
+    const height = rowsOfPixels.length
+    const width = rowsOfPixels[0].length
 
-for (let y = 0; y < outputBMPData.height; y++) {
-    for (let x = 0; x < outputBMPData.width; x ++) {
-        outputBMPData.data.push (
-            rowsOfPixels[y][x].a,
-            rowsOfPixels[y][x].b, 
-            rowsOfPixels[y][x].g, 
-            rowsOfPixels[y][x].r
-        )
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x ++) {
+            data.push (
+                rowsOfPixels[y][x].a,
+                rowsOfPixels[y][x].b, 
+                rowsOfPixels[y][x].g, 
+                rowsOfPixels[y][x].r
+            )
+        }
     }
+    return data
+
+
 }
 // OUTPUT BMP
 
-const outputBMPBuffer = bmp.encode(outputBMPData)
-
-fs.writeFileSync('output.bmp', outputBMPBuffer.data)
+main()
