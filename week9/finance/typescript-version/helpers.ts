546d2fdddc8b0b9d11a955bbfda28fa344fb512e
Luke <lukequattrope@protonmail.com>
2022-10-07 21:33:53 +0100

    updated a few files

--- week9/finance/typescript-version/helpers.ts
+++ week9/finance/typescript-version/helpers.ts
@@ -1,10 +1,13 @@
-const http = require('node:http')
+import fs = require('fs')
 
 module.exports = { 
-
     apology: apology
 }
 
 function apology(message: string, code=400) {
-    
+    let apologyHTML: string = fs.readFileSync("templates/layout.html", "utf-8")
+    console.log(test())
+    apologyHTML.replace(/TITLE/, "Apology")
+   //apologyHTML.replace("{{{}}}")
+   return apologyHTML
 }
