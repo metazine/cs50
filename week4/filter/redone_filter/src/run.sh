a5bf8621e8e0607491b9703fdc072c3de8f78547
Luke <lukequattrope@protonmail.com>
2022-10-08 08:22:14 +0100

    updated run.sh

--- week4/filter/redone_filter/src/run.sh
+++ week4/filter/redone_filter/src/run.sh
@@ -3,7 +3,7 @@
 echo 'What filter are you going to use?: ' &&
 read filterType &&
 echo 'Running filter program...' &&
-node filter.js -$filterType ../test/images/input.bmp && 
+node filter.js -$filterType ./test/images/input.bmp && 
 ffmpeg -i output.bmp -y output.jpeg > temp.txt &&
 rm temp.txt && 
 xdg-open output.jpeg
