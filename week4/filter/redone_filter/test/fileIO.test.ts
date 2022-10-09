6511dfcd1fee2c4768810f6564285c4a41101cbe
Luke <lukequattrope@protonmail.com>
2022-10-09 19:01:16 +0100

    updated fileIO.test.ts

--- week4/filter/redone_filter/test/fileIO.test.ts
+++ week4/filter/redone_filter/test/fileIO.test.ts
@@ -4,9 +4,9 @@ import assert from "assert"
 
 import {writeBuffer, readImageFile} from "../src/fileIO"
 
-test('Should handle input and output', () => {
+test('Should write a file', () => {
     const testBuffer: Buffer = Buffer.from("test", "utf-8")
-    writeBuffer("./realOutput.txt", testBuffer)
+    writeBuffer("../test/realOutput.txt", testBuffer)
     assert.strictEqual(readFileSync("realOutput.txt", "utf-8"), "test")
 })
 
