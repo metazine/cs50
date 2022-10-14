0242e2453d14a013ef7501393d943920fdd666a6
Luke <lukequattrope@protonmail.com>
2022-10-14 22:14:32 +0100

    fixed fileio test

--- week4/filter/redone_filter/test/fileIO.test.ts
+++ week4/filter/redone_filter/test/fileIO.test.ts
@@ -5,25 +5,8 @@ describe("should write a buffer", () => {
 
     it("should be able to write a given buffer", () => {
         const testBuffer: Buffer = Buffer.from("test", "utf-8")
-        writeBuffer("../test/realOutput.txt", testBuffer)
-        const realOutput: string = readFileSync("./realOutput.txt", "utf8")
+        writeBuffer("./test/realOutput.txt", testBuffer)
+        const realOutput: string = readFileSync("./test/realOutput.txt", "utf8")
         expect(realOutput).toStrictEqual("test")
     })
-
 })
-// import test from "node:test"
-// import assert from "assert"
-
-// import {writeBuffer, readImageFile} from "../src/fileIO"
-
-// test('Should write a file', () => {
-//     const testBuffer: Buffer = Buffer.from("test", "utf-8")
-//     writeBuffer("../test/realOutput.txt", testBuffer)
-//     assert.strictEqual(readFileSync("realOutput.txt", "utf-8"), "test")
-// })
-
-// test('Should read a file', () => {
-//     const expectedValue: Buffer = readFileSync("./images/input.bmp")
-//     const actualValue: Buffer = readImageFile("../test/images/input.bmp")
-//     assert.deepStrictEqual(actualValue, expectedValue)
-// })
