ad8e0b0d684c39775154d0c4dd2325bcf628ac6f
Luke <lukequattrope@protonmail.com>
2022-10-14 20:42:35 +0100

    trying and failing to get jest to test typescript

--- week4/filter/redone_filter/test/fileIO.test.ts
+++ week4/filter/redone_filter/test/fileIO.test.ts
@@ -1,17 +1,29 @@
 import {readFileSync} from "fs"
-import test from "node:test"
-import assert from "assert"
+import { writeBuffer } from "../src/fileIO"
 
-import {writeBuffer, readImageFile} from "../src/fileIO"
+describe("should write a buffer", () => {
 
-test('Should write a file', () => {
-    const testBuffer: Buffer = Buffer.from("test", "utf-8")
-    writeBuffer("../test/realOutput.txt", testBuffer)
-    assert.strictEqual(readFileSync("realOutput.txt", "utf-8"), "test")
+    it("should be able to write a given buffer", () => {
+        const testBuffer: Buffer = Buffer.from("test", "utf-8")
+        writeBuffer("../test/realOutput.txt", testBuffer)
+        const realOutput: string = readFileSync("./realOutput.txt", "utf8")
+        expect(realOutput).toStrictEqual("test")
+    })
+
 })
+// import test from "node:test"
+// import assert from "assert"
 
-test('Should read a file', () => {
-    const expectedValue: Buffer = readFileSync("./images/input.bmp")
-    const actualValue: Buffer = readImageFile("../test/images/input.bmp")
-    assert.deepStrictEqual(actualValue, expectedValue)
-})
+// import {writeBuffer, readImageFile} from "../src/fileIO"
+
+// test('Should write a file', () => {
+//     const testBuffer: Buffer = Buffer.from("test", "utf-8")
+//     writeBuffer("../test/realOutput.txt", testBuffer)
+//     assert.strictEqual(readFileSync("realOutput.txt", "utf-8"), "test")
+// })
+
+// test('Should read a file', () => {
+//     const expectedValue: Buffer = readFileSync("./images/input.bmp")
+//     const actualValue: Buffer = readImageFile("../test/images/input.bmp")
+//     assert.deepStrictEqual(actualValue, expectedValue)
+// })
