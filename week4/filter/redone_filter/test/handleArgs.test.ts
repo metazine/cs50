797db2f62ed82d4ab69c77219125567b449ce77a
Luke <lukequattrope@protonmail.com>
2022-10-14 22:17:19 +0100

    fixed handle args test

--- week4/filter/redone_filter/test/handleArgs.test.ts
+++ week4/filter/redone_filter/test/handleArgs.test.ts
@@ -1,11 +1,9 @@
-// import assert from "node:assert"
-// import { describe, it } from "node:test"
-// import handleArgs from "../src/handleArgs"
+import handleArgs from "../src/handleArgs"
 
-// describe("Handle Command Line Args",() => {
-//     it("should reject not 4 element long argv", () => {
-//         const testArgs = ["not", ""]
-//         assert.throws(() => {handleArgs(testArgs)}, /Invalid Arg Count/)
-//     })
+describe("Handle Command Line Args",() => {
+    it("should reject not 4 element long argv", () => {
+        const testArgs = ["not", ""]
+        expect(() => {handleArgs(testArgs)}).toThrow(/Invalid Arg Count/)  
+    })
     
-// })
+})
