54810dfa7e8c7cd7d6e345e8d24762ebed626d2f
Luke <lukequattrope@protonmail.com>
2022-10-15 20:01:49 +0100

    created interfaces.ts

---
+++ week4/filter/redone_filter/src/interfaces.ts
@@ -0,0 +1,20 @@
+export interface Pixel {
+    a: number, // alpha (opacity)
+    r: number, // red
+    g: number, // green
+    b: number // blue
+}
+
+export type Image = Pixel[][]
+
+export interface Bmp {
+    width: number,
+    height: number,
+    data: number[]
+}
+
+export interface ArgvDescriptor {
+    path: string,
+    filterName: string
+}
+
