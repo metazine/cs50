fd728aa1e70de654c6ca41a487ea15fc067bc834
Luke <lukequattrope@protonmail.com>
2022-10-15 09:08:42 +0100

    fixed handleArgs

--- week4/filter/redone_filter/src/handleArgs.ts
+++ week4/filter/redone_filter/src/handleArgs.ts
@@ -1,5 +1,26 @@
-export default function processArgs(argv: string[]) {
-    if (argv.length !== 5) {
+interface argvDescriptor {
+    path: String,
+    filterName: String
+}
+
+export default function processArgs(argv: string[]): argvDescriptor {
+    if (argv.length !== 4) {
         throw new Error("Invalid Arg Count")
     }
+    const filterName: string | undefined = argv[2]
+    const path: string | undefined = argv[3]
+
+    if (!filterName) {
+        throw new Error("Filter name hasn't been given")
+    }
+    if (!path) {
+        throw new Error("Path hasn't been given")
+    } 
+    
+    const argvSpecs: argvDescriptor = {
+        filterName: filterName,
+        path: path
+    }
+    return argvSpecs
+
 }
--- week4/filter/redone_filter/test/handleArgs.test.ts
+++ week4/filter/redone_filter/test/handleArgs.test.ts
@@ -2,7 +2,21 @@ import handleArgs from "../src/handleArgs"
 
 describe("Handle Command Line Args",() => {
     it("should reject not 4 element long argv", () => {
-        const testArgs = ["not", ""]
+        const testArgs = ["1", "2"]
         expect(() => {handleArgs(testArgs)}).toThrow(/Invalid Arg Count/)  
+    })
+
+    it ("should accept 4 element long argv", () => {
+        const testArgs = ["1","2","3","4"]
+        expect(handleArgs(testArgs)).toBeTruthy()
+    })
+
+    it("Returns the name of the filter", () => {
+        const testArgs = ["node", "index.js", "filterName", "path"]
+        const expectedOutput = {
+            filterName: "filterName",
+            path: "path"
+        }
+        expect(handleArgs(testArgs)).toMatchObject(expectedOutput)
     })
 })
