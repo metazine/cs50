b3a14861b4af51cd9f8605a027a32819314baa72
Luke <lukequattrope@protonmail.com>
2022-10-15 09:13:25 +0100

    fixed bug in fileIO.test.ts

--- week4/filter/redone_filter/test/fileIO.test.ts
+++ week4/filter/redone_filter/test/fileIO.test.ts
@@ -1,11 +1,13 @@
-import {readFileSync} from "fs"
+import {readFileSync, unlinkSync} from "fs"
 import { writeBuffer } from "../src/fileIO"
 
 describe("should write a buffer", () => {
     it("should be able to write a given buffer", () => {
+        const path: string = "./test/realOutput.txt"
         const testBuffer: Buffer = Buffer.from("test", "utf-8")
-        writeBuffer("./test/realOutput.txt", testBuffer)
-        const realOutput: string = readFileSync("./test/realOutput.txt", "utf8")
+        writeBuffer(path, testBuffer)
+        const realOutput: string = readFileSync(path, "utf8")
+        unlinkSync(path)
         expect(realOutput).toStrictEqual("test")
     })
 })
