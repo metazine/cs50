990a004b7a0d2bdb481ce332cc3bd3dac9b7e9b0
metazine <lukequattrope@protonmail.com>
2022-10-20 16:01:56 +0100

    removed ine break in filter.test.ts

--- week4/filter/redone_filter/test/filter.test.ts
+++ week4/filter/redone_filter/test/filter.test.ts
@@ -1,7 +1,6 @@
 import filter from "../src/filter"
 import testImage from "./testImage"
 
-
 describe("filter", () => {
     it ("Rejects unknown filters", () => {
         expect(() => {filter("unknown", testImage.input)}).toThrowError(/Unknown filter type/)
@@ -10,7 +9,7 @@ describe("filter", () => {
     it ("Runs greyscale filter properly", () => {
         expect(filter("greyscale", testImage.input)).toStrictEqual(testImage.greyScaleExpectedOutput)
     })
-    
+
     it ("Runs edge detection filter properly", () => {
         expect(filter("edgeDetection", testImage.input)).toStrictEqual(testImage.edgeDetectionExpectedOutput)
     })
