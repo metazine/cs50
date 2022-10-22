197a284cba0f47005a8b8ab848b8fe0f336108d6
metazine <lukequattrope@protonmail.com>
2022-10-22 18:35:42 +0100

    created getPixel implementation interfaces.ts

--- week4/filter/redone_filter/src/interfaces.ts
+++ week4/filter/redone_filter/src/interfaces.ts
@@ -5,7 +5,38 @@ export interface Pixel {
     b: number // blue
 }
 
-export type Image = Pixel[][]
+export type ImageData = Pixel[][]
+
+interface Image {
+    getPixel(x: number, y: number): Pixel,
+    setPixel(x: number, y: number, pixel: Pixel): Pixel,
+    width: number,
+    height: number,
+    data: ImageData
+}
+
+class pixelArrayImage implements Image {
+    width: number
+    height: number
+    data: ImageData
+    constructor(imageData: ImageData) {
+        this.data = imageData
+        this.height =  this.data.length
+        this.width = this.data[0]?.length || 0  
+    }
+    getPixel(x: number, y: number): Pixel {
+        let pixelValue: Pixel | undefined  = this.data[y]?.[x]
+        if (pixelValue === undefined) {
+            throw new Error(`Position ${x}, ${y} doesn't exist`)
+        }
+        return pixelValue
+    }
+    setPixel(x: number, y: number, pixel: Pixel): Pixel {
+        
+    }
+}
+
+
 
 export type Kernel = readonly number[][]
 
