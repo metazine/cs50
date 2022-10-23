f3d9b816394fd91670429e98cf45935d5eea876c
metazine <lukequattrope@protonmail.com>
2022-10-23 19:59:55 +0100

    removed unnecessary type definitions

--- week4/filter/redone_filter/src/applyKernel.ts
+++ week4/filter/redone_filter/src/applyKernel.ts
@@ -4,7 +4,7 @@ export default function applyKernel(kernel: Kernel, input: Image) {
     const kernelSize: number = kernel.length
     const kernelMiddle: number = (kernelSize - 1) / 2
 
-    let kernelTotal: number = 0;
+    let kernelTotal = 0;
     kernel.forEach((row) => {
         row.forEach((value) => {
             kernelTotal += value
@@ -19,16 +19,16 @@ export default function applyKernel(kernel: Kernel, input: Image) {
 
     let output = new PixelArrayImage(input.data)
 
-    for (let y: number = 0; y < input.height; y++) {
-        for (let x: number = 0; x < input.width; x++) {
+    for (let y = 0; y < input.height; y++) {
+        for (let x = 0; x < input.width; x++) {
             const pixel: Pixel = {
                 a: 0,
                 r: 0,
                 g: 0,
                 b: 0
             }
-            for (let i: number = -kernelMiddle; i <= kernelMiddle; i++) {
-                for (let j: number = -kernelMiddle; j <= kernelMiddle; j++) {
+            for (let i = -kernelMiddle; i <= kernelMiddle; i++) {
+                for (let j = -kernelMiddle; j <= kernelMiddle; j++) {
                     const relativePixel: Pixel | undefined = input.getPixel(x + j, y + i)
                     const kernelValue: number = kernel[j + 1]?.[i + 1] || 0
 
