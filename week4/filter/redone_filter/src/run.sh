cfbce46b35a0c174729e2dcb5cfaf62520ff8c2d
metazine <lukequattrope@protonmail.com>
2022-10-26 09:28:32 +0100

    removed run.sh

--- week4/filter/redone_filter/src/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'What filter are you going to use?: ' &&
-read filterType &&
-echo 'Running filter program...' &&
-node ./src/index.js -$filterType ./test/images/input.bmp && 
-ffmpeg -i output.bmp -y output.jpeg > temp.txt &&
-rm temp.txt && 
-xdg-open output.jpeg
